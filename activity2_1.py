b553775a2fc09e609aec09fc4d6391280db8e7f5
derweg7563-ctrl <derweg7563@gmail.com>
2026-09-07 22:58:42 +0900

    Update activity2_1.py

--- activity2_1.py
+++ activity2_1.py
@@ -98,7 +98,7 @@ def generate_ai_desc(relic_name):
 # 🏛️ 기능 2: 국립중앙박물관 유물 검색기
 # ==========================================
 def search_museum_relics(keyword):
-    url = "http://www.emuseum.go.kr/openapi/relic/list"
+    url = "http://www.emuseum.go.kr/openapi/relic/list"# https는 지원 안 됨
     my_key = st.secrets["museum"]["api_key"]
 
     headers = {
